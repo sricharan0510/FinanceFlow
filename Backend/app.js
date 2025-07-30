aada162ae065f7b7d4f1426e8bec837954edb51e
Sricharan Illandula <sricharan0504@gmail.com>
2025-07-30 10:10:59 +0530

    Refactor code structure for improved readability and maintainability

--- Backend/app.js
+++ Backend/app.js
@@ -10,9 +10,20 @@ dotenv.config({
 
 const app = express();
 
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://finance-flow-flame-nu.vercel.app'
+];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
 }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
